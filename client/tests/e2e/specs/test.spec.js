959edd9d97079d127cff9adc39b1d29a78e3329e
Andres.O <and.rews@hotmail.com>
2020-02-04 17:31:17 +0100

    add cypress e2e tests

--- client/tests/e2e/specs/test.spec.js
+++ client/tests/e2e/specs/test.spec.js
@@ -1,30 +1,30 @@
 
-// describe('Login user and create a new Post', () => {
-//     it("Upload a Post", ()=> {
-//         cy.visit('http://localhost:8080/');
-//         cy.get("#loginBtn").click();
-//         cy.get("#usernameLoginInput").type("blogUser");
-//         cy.get("#passwordLoginInput").type("123");
-//         cy.get("#loginBtnSubmit").click();
-//         cy.get("#myPosts").click();
-//         cy.get("#titlePostInput").type("TITULO DESDE CYPRESS");
-//         cy.get("#textPostTextarea").type("TEXTO DEL POST DESDE CYPRESS")
-//         cy.get("#uploadPostButton").click();
-//     });
-//   })
+describe('Login user and create a new Post', () => {
+    it("Upload a Post", ()=> {
+        cy.visit('http://localhost:8080/');
+        cy.get("#loginBtn").click();
+        cy.get("#usernameLoginInput").type("blogUser");
+        cy.get("#passwordLoginInput").type("123");
+        cy.get("#loginBtnSubmit").click();
+        cy.get("#myPosts").click();
+        cy.get("#titlePostInput").type("TITULO DESDE CYPRESS");
+        cy.get("#textPostTextarea").type("TEXTO DEL POST DESDE CYPRESS")
+        cy.get("#uploadPostButton").click();
+    });
+  })
 
-//   describe('Login user and create new comment', () => {
-//     it("Create comment WITHOUT offensive words", ()=> {
-//         cy.visit('http://localhost:8080/');
-//         cy.get("#loginBtn").click();
-//         cy.get("#usernameLoginInput").type("blogUser");
-//         cy.get("#passwordLoginInput").type("123");
-//         cy.get("#loginBtnSubmit").click();
-//         cy.get("#5e31b67e693ff45ff59af7bc").click();
-//         cy.get("#textCommentTextarea").type("COMENTARIO DESDE CYPRESS SIN PALABRAS OFENSIVAS");
-//         cy.get("#btn-comment").click();
-//     });
-//   });
+  describe('Login user and create new comment', () => {
+    it("Create comment WITHOUT offensive words", ()=> {
+        cy.visit('http://localhost:8080/');
+        cy.get("#loginBtn").click();
+        cy.get("#usernameLoginInput").type("blogUser");
+        cy.get("#passwordLoginInput").type("123");
+        cy.get("#loginBtnSubmit").click();
+        cy.get("#5e31b67e693ff45ff59af7bc").click();
+        cy.get("#textCommentTextarea").type("COMENTARIO DESDE CYPRESS SIN PALABRAS OFENSIVAS");
+        cy.get("#btn-comment").click();
+    });
+  });
 
 describe('Login user and create new comment', () => {
     it("Create comment WITH offensive words", ()=> {
